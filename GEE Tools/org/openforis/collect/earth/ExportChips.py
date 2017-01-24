a0fb13920703a8a97a1b175518698d8ea16a2c27
herrtunante <herrtunante@users.noreply.github.com>
2017-01-24 13:36:30 +0100

    added pansharpening

--- GEE Tools/org/openforis/collect/earth/ExportChips.py
+++ GEE Tools/org/openforis/collect/earth/ExportChips.py
@@ -28,7 +28,7 @@ plots_for_chips = plots.map(lambda f: f.buffer(2000))
 plots_for_indices = plots.map(lambda f: f.buffer(35))
 
 #Where is the work carried out ?
-country_name = 'CubaxxxFinal';
+country_name = 'CubaImprovedV1';
 
 #Do you want to export the roi OR the convex hull of the ROI ? 
 export_roi = "yes";
@@ -48,50 +48,48 @@ cloudcovthres = 5;
 #ee.mapclient.addToMap(small_counties, {'color': '900000'})
 
 #//////////////////// Create functions to harmonize band names
-my_band_names = ['B3','B4','B5','B6','B7'];
+my_band_names = ['B2','B3','B4','B5','B6','B7', 'B8','B10', 'B11'];
 
 def harmonize_l57(image):
-    return image.select(['B2','B3', 'B4', 'B5', 'B7'], my_band_names)
+    return image.select(['B1','B2','B3','B4','B5','B7','B8','B6_VCID_1','B6_VCID_2'], my_band_names)
 
 def harmonize_l8(image):
-    return image.select(['B3','B4', 'B5', 'B6', 'B7'], my_band_names)
+    return image.select(['B2','B3','B4','B5','B6','B7','B8','B10','B11'], my_band_names)
 
-sentinelVisBands = ['B4_mean','B3_mean','B2_mean'];
+sentinelVisBands = ['B8_mean','B4_mean','B3_mean'];
+l7VisBands = ['B5_mean', 'B6_mean', 'B4_mean'];
 
 def cloudMask_jrc(im):
     """Implement Sentinel cloud mask
     Opaque and cirrus cloud masks cause bits 10 and 11 in QA60 to be set,
     so values less than 1024 are cloud-free
     """
-    cloud1 = ee.Image(0).where(im.select('QA60').gte(1024), 1)
-    cloud2 = ee.Image(0).where(im.select('B2').gte(1200), 1)
-    cloud2b = cloud2.focal_min(200,'circle','meters')
-    #compute the n of cloudfree observations
-    clouds=((cloud1.eq(1)).Or(cloud2b.eq(1)))
-    im = im.addBands(clouds.eq(0).select([0],["cloudfree"])); # and add a band with this information
-    return im.updateMask(clouds.eq(0));
-
-l7VisBands = ['B5_mean', 'B6_mean', 'B4_mean'];
-
+    mask = ee.Image(0).where(im.select('QA60').gte(1024), 1).Not();
+    return im.updateMask(mask);
+#    cloud1 = ee.Image(0).where(im.select('QA60').gte(1024), 1)
+#     cloud2 = ee.Image(0).where(im.select('B2').gte(1200), 1)
+#     cloud2b = cloud2.focal_min(200,'circle','meters')
+#     #compute the n of cloudfree observations
+#     clouds=((cloud1.eq(1)).Or(cloud2b.eq(1)))
+#     im = im.addBands(clouds.eq(0).select([0],["cloudfree"])); # and add a band with this information
+#     return im.updateMask(clouds.eq(0));
 
 def generateChips(chip):
     # Which years ?
     for year in range(year_start,year_end):
         print(year);
         
-        tmin = str(year) + '-01-01';
-        tmax = str(year) + '-12-31';
+        tmin = str(year) + '-11-01';
+        tmax = str(year+1) + '-04-30';
     
-    #     /////////////////////////////////
-    #     // Combine FILTERS           ////
-    #     /////////////////////////////////
-    
+        #     /////////////////////////////////
+        #     // Combine FILTERS           ////
+        #     /////////////////////////////////
         #Create a location filter
         locFilter = ee.Filter.geometry(chip.geometry() )
     
         # Create a range of date to filter imagery to use
         dateFilter = ee.Filter.date( Date(tmin), Date(tmax) )
-    
     
         dateLocFilter = ee.Filter.And(locFilter,dateFilter)
     
@@ -103,64 +101,54 @@ def generateChips(chip):
         combinedyFilter = ee.Filter.And(dateLocFilter,doyFilter, cloudCoverFilter);
         
         
-    #     /////////////////////////////////
-    #     // Select Imagery | Filters  ////
-    #     /////////////////////////////////
-    #     // Look into the archives and select the imagery following the filters
-        inputl5 = ee.ImageCollection('LT5_L1T_TOA').filter(combinedyFilter);
-        inputl7 = ee.ImageCollection('LE7_L1T_TOA').filter(combinedyFilter);
-        inputl8 = ee.ImageCollection('LC8_L1T_TOA').filter(combinedyFilter);
+        #     /////////////////////////////////
+        #     // Select Imagery | Filters  ////
+        #     /////////////////////////////////
+        #     // Look into the archives and select the imagery following the filters
+        inputl5 = ee.ImageCollection('LANDSAT/LT5_L1T_TOA').filter(combinedyFilter);
+        inputl7 = ee.ImageCollection('LANDSAT/LE7_L1T_TOA').filter(combinedyFilter);
+        inputl8 = ee.ImageCollection('LANDSAT/LC8_L1T_TOA').filter(combinedyFilter);
     
-    #     /////////////////////////////////////////
-    #     //// Check availability of archives /////
-    #     /////////////////////////////////////////
+        #     /////////////////////////////////////////
+        #     //// Check availability of archives /////
+        #     /////////////////////////////////////////
         count_l5 = inputl5.aggregate_count('system:index')
         count_l7 = inputl7.aggregate_count('system:index')
         count_l8 = inputl8.aggregate_count('system:index')
         
         
-    #     /////////////////////////////////////////
-    #     //// Normalize band names           /////
-    #     /////////////////////////////////////////
+        #     /////////////////////////////////////////
+        #     //// Normalize band names           /////
+        #     /////////////////////////////////////////
         l5 = inputl5.map(harmonize_l57)
         l7 = inputl7.map(harmonize_l57)
         l8 = inputl8.map(harmonize_l8)
     
-    #     /////////////////////////////////////////
-    #     //// Merge 3 collections into one   /////
-    #     /////////////////////////////////////////
+        #     /////////////////////////////////////////
+        #     //// Merge 3 collections into one   /////
+        #     /////////////////////////////////////////
         merged = ee.ImageCollection(l5.merge(l7).merge(l8))
        
-       
-    #     /////////////////////////////////////////
-    #     //// Create median values           /////
-    #     /////////////////////////////////////////
-        median_al = merged.reduce( ee.Reducer.intervalMean(25,75) )
-        
-    #     //Changed addToMap() it became a deprecated method  since September 2016, instead use Map.addLayer() 
-        
-    #     Map.addLayer(median_al.clipToCollection(roi),{bands: l7VisBands, max: 0.3}, country_name + '_median_roi_'+year,false);
-    #     ////////////////////////////////////////////////////////////////////////////////////////////////
-    #     ///////////////////// EXPORT RESULTS USING THE COUNTRY SHAPE YOU SPECIFIED        /////////////
-    #     ////////////////////////////////////////////////////////////////////////////////////////////////
-    
-        # Call the sentinel 2 collection
-        inputs2 = ee.ImageCollection('COPERNICUS/S2').filter(dateLocFilter);
-        
-        # Mask out the clouds
-        s2clouds=inputs2.map(cloudMask_jrc);
-        
-        # Take only the median observations and select 10m bands
-        median_s2 = s2clouds.select(['B2','B3','B4','B8']).reduce(ee.Reducer.intervalMean(25,75));
-        
-        
+        #     /////////////////////////////////////////
+        #     //// Create median values           /////
+        #     /////////////////////////////////////////
+        #median_al = merged.reduce( ee.Reducer.intervalMean(25,75) )
+                  
         idChip = chip.get('id').getInfo() 
-        print (idChip)
         
-    #     ////////////////////////////////////////////////////////////////////////////////////////////////
-    #     ///////////////////// EXPORT RESULTS USING THE DOWNLOAD GRID YOU SPECIFIED         /////////////
-    #     ////////////////////////////////////////////////////////////////////////////////////////////////
+        #     ////////////////////////////////////////////////////////////////////////////////////////////////
+        #     ///////////////////// EXPORT RESULTS USING THE DOWNLOAD GRID YOU SPECIFIED         /////////////
+        #     ////////////////////////////////////////////////////////////////////////////////////////////////
         if export_roi == 'yes' and year >= 2015:
+            # Call the sentinel 2 collection
+            inputs2 = ee.ImageCollection('COPERNICUS/S2').filter(dateLocFilter).filter(ee.Filter.lte('CLOUDY_PIXEL_PERCENTAGE', 30) )
+        
+            # Mask out the clouds
+            s2clouds=inputs2.map(cloudMask_jrc);
+        
+            # Take only the median observations and select 10m bands
+            median_s2 = s2clouds.select(['B2','B3','B4','B8']).reduce(ee.Reducer.intervalMean(25,75));
+        
             s2task = ee.batch.Export.image.toDrive(
                                  median_s2.select(sentinelVisBands).clip(chip.geometry() ), 
                                  description = 'median_roi_clip_s2_bb_'+country_name ,#+ "_" + str( chip.get('id') ), 
@@ -175,7 +163,7 @@ def generateChips(chip):
     
         if export_roi == 'yes' :
             landsatTask = ee.batch.Export.image.toDrive( 
-                                 median_al.select(l7VisBands).clip(chip.geometry() ), 
+                                 panSharpen(merged).select(l7VisBands).clip(chip.geometry() ), 
                                  description= '_median_roi_clip_'+ str(year) +'_bb_'+ country_name , # "_" + str( chip.get('id') ), 
                                  scale= 30, 
                                  folder = country_name+'_Landsat_Chips_' + str(year) , 
@@ -185,6 +173,45 @@ def generateChips(chip):
             
             print ( landsatTask.status()['id'] )
             
+
+def panSharpen( landsatHarmonizedcollection ):
+
+    # Mask clouds by mapping the cloudMask function over the collection.
+    # This will add a cloud score band called 'cloud' to every image.
+    collection = landsatHarmonizedcollection.map(lambda f: ee.Algorithms.Landsat.simpleCloudScore(f) );
+
+    # Convert the collection to an array.
+    array = collection.toArray();
+
+    # Label of the axes.
+    imageAxis = 0;
+    bandAxis = 1;
+
+    # Get the cloud slice and the bands of interest.
+    bands = array.arraySlice(bandAxis, 0, len(my_band_names))
+    clouds = array.arraySlice(bandAxis, len(my_band_names))
+
+    # Sort by cloudiness.
+    sortedArray = bands.arraySort(clouds)
+
+    # Get the least cloudy images, 20% of the total.
+    numImages = sortedArray.arrayLength(imageAxis).multiply(0.2).int()
+    leastCloudy = sortedArray.arraySlice(imageAxis, 0, numImages)
+
+    # Get the mean of the least cloudy images by reducing along the image axis.
+    mean = leastCloudy.arrayReduce(reducer= ee.Reducer.mean(), axes = [imageAxis])
+    
+    bandNames = ee.List(my_band_names);
+    
+    # Turn the reduced array image into a multi-band image for display.
+    meanImage = mean.arrayProject([bandAxis]).arrayFlatten([bandNames])
+    # Convert the RGB bands to the HSV color space.
+    hsv = meanImage.select( l7VisBands ).rgbToHsv()
+
+    # Swap in the panchromatic band and convert back to RGB.
+    sharpened = ee.Image.cat([hsv.select('hue'), hsv.select('saturation'), meanImage.select('B8')]).hsvToRgb()
+
+    return sharpened
 
 
 #plots_for_chips.map( generateChips );
